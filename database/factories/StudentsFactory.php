8987aa6c442bfc2dac5d36c9dea61ada61e0f3f6
Louie <louiec@callboxinc.com>
2023-02-16 15:30:10 +0800

    configuring database

---
+++ database/factories/StudentsFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Students>
+ */
+class StudentsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'age' => fake()->name(),
+            'gender' => fake()->name(),
+            'email' => fake()->unique()->safeEmail()
+        ];
+    }
+}
